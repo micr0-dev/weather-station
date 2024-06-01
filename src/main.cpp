aafecee0f7767b1165296ab34a3714ddf8d3cb9c
Micr0Byte <26364458+micr0-dev@users.noreply.github.com>
2024-06-01 00:58:30 -0400

    TSL2591 improvements

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,9 @@ SemaphoreHandle_t bufferMutex;
 bool enableSHT4x = false;
 bool enableTSL2591 = true;
 
+// Luminosity level
+uint8_t TSL2591level = 1;
+
 void get_network_info()
 {
     if (WiFi.status() == WL_CONNECTED)
@@ -36,6 +39,110 @@ void get_network_info()
         Serial.println((String) "[+] RSSI : " + WiFi.RSSI() + " dB");
         Serial.print("[+] ESP32 IP : ");
         Serial.println(WiFi.localIP());
+    }
+}
+
+void configureSensors()
+{
+    if (enableSHT4x)
+    {
+        Serial.println("Adafruit SHT4x test");
+        if (!sht4.begin())
+        {
+            Serial.println("Couldn't find SHT4x");
+            while (1)
+                delay(1);
+        }
+        Serial.println("Found SHT4x sensor");
+        Serial.print("Serial number 0x");
+        Serial.println(sht4.readSerial(), HEX);
+        sht4.setPrecision(SHT4X_HIGH_PRECISION);
+        sht4.setHeater(SHT4X_NO_HEATER);
+    }
+
+    if (enableTSL2591)
+    {
+        if (!tsl.begin())
+        {
+            Serial.println("Couldn't find TSL2591");
+            while (1)
+                delay(1);
+        }
+        Serial.println("Found TSL2591 sensor");
+        Serial.print("ID 0x");
+        Serial.println(2591, HEX);
+        tsl.setGain(TSL2591_GAIN_MED);                // Default gain
+        tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS); // Default integration time
+    }
+}
+
+void configureTSL2591()
+{
+    tsl.setGain(TSL2591_GAIN_MED);                // Default gain
+    tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS); // Default integration time
+
+    Serial.println("------------------------------------");
+    Serial.print("Gain:          ");
+    tsl2591Gain_t gain = tsl.getGain();
+
+    Serial.print("Timing:        ");
+    Serial.print((tsl.getTiming() + 1) * 100, DEC);
+    Serial.println(" ms");
+    Serial.println("------------------------------------");
+    Serial.println("");
+}
+
+void adjustTSL2591Settings(uint16_t initLuminosity)
+{
+    Serial.print("Initial luminosity: ");
+    Serial.println(initLuminosity);
+
+    bool adjust = false;
+    if (initLuminosity < 10 && TSL2591level != 5)
+    {
+        TSL2591level++;
+        adjust = true;
+    }
+    else if (initLuminosity > 100 && TSL2591level != 0)
+    {
+        TSL2591level--;
+        adjust = true;
+    }
+
+    if (adjust)
+    {
+        Serial.print("Adjusting TSL2591 settings to level ");
+        Serial.println(TSL2591level);
+
+        switch (TSL2591level)
+        {
+        case 0:
+            tsl.setGain(TSL2591_GAIN_LOW);
+            tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);
+            break;
+        case 1:
+            tsl.setGain(TSL2591_GAIN_MED);
+            tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);
+            break;
+        case 2:
+            tsl.setGain(TSL2591_GAIN_HIGH);
+            tsl.setTiming(TSL2591_INTEGRATIONTIME_200MS);
+            break;
+        case 3:
+            tsl.setGain(TSL2591_GAIN_MAX);
+            tsl.setTiming(TSL2591_INTEGRATIONTIME_200MS);
+            break;
+        case 4:
+            tsl.setGain(TSL2591_GAIN_MAX);
+            tsl.setTiming(TSL2591_INTEGRATIONTIME_400MS);
+            break;
+        case 5:
+            tsl.setGain(TSL2591_GAIN_MAX);
+            tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);
+            break;
+        default:
+            break;
+        }
     }
 }
 
@@ -69,18 +176,28 @@ void collectDataTask(void *pvParameters)
 
         if (enableTSL2591)
         {
-            uint16_t x = tsl.getLuminosity(TSL2591_VISIBLE);
+            // Adjust settings based on initial luminosity
+
+            uint32_t lum = tsl.getFullLuminosity();
+            uint16_t ir, full, visible;
+            ir = lum >> 16;
+            full = lum & 0xFFFF;
+            visible = full - ir;
+
+            float lux = tsl.calculateLux(full, ir);
+
             Serial.print("[ ");
             Serial.print(millis());
             Serial.print(" ms ] ");
-            Serial.print("Luminosity: ");
-            Serial.print(x);
-            Serial.println(" Lux");
+            Serial.print("Lux: ");
+            Serial.println(lux);
+
+            adjustTSL2591Settings(visible);
 
             // Collect data into buffer
             if (xSemaphoreTake(bufferMutex, portMAX_DELAY) == pdTRUE)
             {
-                String data = "{\"sensor\":\"TSL2591\", \"luminosity\": " + String(x) + "}";
+                String data = "{\"sensor\":\"TSL2591\", \"lux\": " + String(lux) + "}";
                 if (dataBuffer.length() > 0)
                 {
                     dataBuffer += ",";
@@ -131,72 +248,6 @@ void sendDataTask(void *pvParameters)
     }
 }
 
-void configureSensors()
-{
-    if (enableSHT4x)
-    {
-        Serial.println("Adafruit SHT4x test");
-        if (!sht4.begin())
-        {
-            Serial.println("Couldn't find SHT4x, disabling TSL2591 sensor");
-            enableSHT4x = false;
-        }
-        else
-        {
-            Serial.println("Found SHT4x sensor");
-            Serial.print("Serial number 0x");
-            Serial.println(sht4.readSerial(), HEX);
-            sht4.setPrecision(SHT4X_HIGH_PRECISION);
-            sht4.setHeater(SHT4X_NO_HEATER);
-        }
-    }
-
-    if (enableTSL2591)
-    {
-        if (!tsl.begin())
-        {
-            Serial.println("Couldn't find TSL2591, disabling TSL2591 sensor");
-            enableTSL2591 = false;
-        }
-        else
-        {
-            Serial.println("Found TSL2591 sensor");
-            configureTSL2591();
-        }
-    }
-}
-
-void configureTSL2591()
-{
-    tsl.setGain(TSL2591_GAIN_MED);
-    tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);
-
-    Serial.println("------------------------------------");
-    Serial.print("Gain:          ");
-    tsl2591Gain_t gain = tsl.getGain();
-    switch (gain)
-    {
-    case TSL2591_GAIN_LOW:
-        Serial.println("1x (Low)");
-        break;
-    case TSL2591_GAIN_MED:
-        Serial.println("25x (Medium)");
-        break;
-    case TSL2591_GAIN_HIGH:
-        Serial.println("428x (High)");
-        break;
-    case TSL2591_GAIN_MAX:
-        Serial.println("9876x (Max)");
-        break;
-    }
-
-    Serial.print("Timing:        ");
-    Serial.print((tsl.getTiming() + 1) * 100, DEC);
-    Serial.println(" ms");
-    Serial.println("------------------------------------");
-    Serial.println("");
-}
-
 void setup()
 {
     Serial.begin(115200);
@@ -210,19 +261,7 @@ void setup()
     }
     Serial.println("Connected to WiFi");
 
-    Serial.println("Adafruit SHT4x test");
-    if (!sht4.begin())
-    {
-        Serial.println("Couldn't find SHT4x");
-        while (1)
-            delay(1);
-    }
-    Serial.println("Found SHT4x sensor");
-    Serial.print("Serial number 0x");
-    Serial.println(sht4.readSerial(), HEX);
-
-    sht4.setPrecision(SHT4X_HIGH_PRECISION);
-    sht4.setHeater(SHT4X_NO_HEATER);
+    get_network_info();
 
     bufferMutex = xSemaphoreCreateMutex();
     if (bufferMutex == NULL)
@@ -231,6 +270,8 @@ void setup()
         while (1)
             delay(1);
     }
+
+    configureSensors();
 
     xTaskCreatePinnedToCore(collectDataTask, "Collect Data Task", 8192, NULL, 1, NULL, 0);
     xTaskCreatePinnedToCore(sendDataTask, "Send Data Task", 8192, NULL, 1, NULL, 1);
